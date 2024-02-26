812d27b48dfb9c092750055b0312f880eebe5e1b
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-26 01:49:38 +0100

    f added

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,43 +2,52 @@ import validator from 'validator';
 
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', inputHandler);
+// form.addEventListener('input', inputHandler);
 
 let savedfeedbackMessage = JSON.parse(
   localStorage.getItem('feedback-form-state')
 );
 
-function addSavedInformation(obj) {
-  if (obj !== null) {
-    console.log('this is form message: ', (form.message.value = obj.message));
-    console.log('This is form email: ', (form.email.value = obj.email));
+function addSavedInformation({ email = '', message = '' }) {
+  {
+    console.log('this is form message: ', (form.message.value = message));
+    console.log('This is form email: ', (form.email.value = email));
   }
   return;
 }
+if (savedfeedbackMessage !== null) {
+  addSavedInformation(savedfeedbackMessage);
+}
 
-addSavedInformation(savedfeedbackMessage);
+console.log('This is saved feedback message: ', savedfeedbackMessage);
 
-console.log(
-  'This is saved feedback message: ',
-  savedfeedbackMessage,
-  typeof savedfeedbackMessage
-);
-console.log('This is null: ', typeof null);
 let feedbackMessage = {};
+
+form.addEventListener('input', inputHandler);
+
 function inputHandler(event) {
-  // console.log(event.target.value);
-  // console.log('This is event in input: ', event.target.elements.message.value);
-  // - почему не работает ?
+  event.preventDefault();
+  let value = event.target.value;
   event.target.nodeName === 'INPUT'
-    ? (feedbackMessage.email = event.target.value)
-    : (feedbackMessage.message = event.target.value);
+    ? (feedbackMessage.email = value.trim())
+    : (feedbackMessage.message = value.trim());
 
   localStorage.setItem('feedback-form-state', JSON.stringify(feedbackMessage));
 }
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', submitHandler);
+
+function submitHandler(event) {
   event.preventDefault();
-  console.log('This is event in submit: ', event.target.elements.message.value);
-  localStorage.removeItem('feedback-form-state');
-  form.reset();
-});
+  if (inputCheck(feedbackMessage)) {
+    console.log(feedbackMessage);
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+    feedbackMessage = {};
+  } else alert('There is no email or message');
+}
+
+function inputCheck({ email = '', message = '' }) {
+  console.log('This email and message from input check: ', email, message);
+  return validator.isEmail(email) && !validator.isEmpty(message);
+}
